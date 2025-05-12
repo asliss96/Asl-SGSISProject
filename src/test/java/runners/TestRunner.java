1da1c146955c65ebe4ca228cbad1e3d83e521682
asliss <aslisaracoglugurpinar@gmail.com>
2025-05-12 19:35:25 +0300

    First Commit

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,41 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+	
+		features="src/test/resources/feature/",
+
+		
+		glue="steps", 
+		
+		
+		
+		dryRun=false, 
+		
+		
+		monochrome=true, 
+		
+		
+		tags= "@Login",
+		
+		plugin = {
+					
+					"pretty",
+					
+					"html:target/cucumber-default-report.html",
+					
+					"json:target/cucumber.json"
+				}
+				
+		
+		)
+
+public class TestRunner {
+
+}
